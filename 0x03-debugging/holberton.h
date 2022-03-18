9700245f8c9d5695667610f4008d328bd4aa3a69
root <geletay@gmail.com>
2022-03-18 13:51:27 -0700

    second push

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day)
+
+#endif /* HOLBERTON_H */
+  
