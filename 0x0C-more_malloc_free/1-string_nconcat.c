fbd4c9d1e00f8df5cdf2250babec1234ce33dd41
root <geletay@gmail.com>
2022-04-07 19:35:19 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
 /**
-* string_nconcat - function that concatenates two strings.
-* @s1: the first string
-* @s2: the second string
-* @n: the second string length
-* Return: concatenated string
+* _strlen - calculate and return string length
+* @string: string
+* Return: string length
+*/
+int _strlen(char *string)
+{
+int i;
+for (i = 0; string[i] != '\0'; i++)
+;
+return (i);
+}
+/**
+* string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+* @s1: string 1
+* @s2: string 2
+* @n: n bytes to concat from string 2
+* Return: pointer to concatenated string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *string;
-unsigned int i = 0;
-unsigned int j = 0;
+char *ptr;
+int num, len, i, j;
+num = n;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-string = malloc(sizeof(s1) + n + 1);
-if (string == NULL)
+if (num < 0)
 return (NULL);
-while (s1[i] != '\0')
-{
-string[i] = s1[i];
-i++;
-}
-while (j < n)
-{
-string[i] = s2[j];
-i++;
-j++;
-}
-string[i] = '\0';
-return (string);
+if (num >= _strlen(s2))
+num = _strlen(s2);
+len = _strlen(s1) + num + 1;
+ptr = malloc(sizeof(*ptr) * len);
+if (ptr == NULL)
+return (NULL);
+for (i = 0; s1[i] != '\0'; i++)
+ptr[i] = s1[i];
+for (j = 0; j < num; j++)
+ptr[i + j] = s2[j];
+ptr[i + j] = '\0';
+return (ptr);
 }
